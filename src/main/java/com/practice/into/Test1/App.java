82423ab4e98089e0487231e22b65fd9834c5e8d2
baby bai <baby.bai@corp.litmusworld.com>
2020-04-21 17:41:46 +0530

    First commit

---
+++ src/main/java/com/practice/into/Test1/App.java
@@ -0,0 +1,15 @@
+package com.practice.into.Test1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "HOW ARE U!" );
+        
+    }
+}
